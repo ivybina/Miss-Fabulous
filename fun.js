8406c2a02d6c6f330e70a0381982305e1e3ec4c4
ivybina <ivybina999@gmail.com>
2017-08-21 19:45:36 +0300

    change of names

--- fun.js
+++ fun.js
@@ -15,16 +15,19 @@ var shop = [
     name: "Black Ruby",
     price: 1400,
     quantity: 0
+    // image:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2Fcb%2F23%2F3f%2Fcb233fd567609bba839b87d06a6dd3b0--purple-wedding-shoes-high-heel-pumps.jpg&imgrefurl=https%3A%2F%2Fwww.pinterest.com%2Fexplore%2Fhigh-heels-2014%2F&docid=UOpAyUCXOYtoPM&tbnid=Yp4r-6Hq9x0ekM%3A&vet=10ahUKEwid5qjjsePVAhUFWRQKHSVxD4M4ZBAzCC4oKzAr..i&w=736&h=736&bih=613&biw=1366&q=heels&ved=0ahUKEwid5qjjsePVAhUFWRQKHSVxD4M4ZBAzCC4oKzAr&iact=mrc&uact=8",
   },
   {
     name: "Heels",
     price: 1200,
     quantity: 0
+    // image:"shoes/heel2.jpg",
   },
   {
     name: "Flower Ruby",
     price: 1400,
     quantity: 0
+    // image:"shoes/boot1.jpg",
   },
 ];
 
